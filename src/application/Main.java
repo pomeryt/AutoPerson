ac426a9c3696e117ea4fc210ff23c9704dd6fee2
Rin <pomeryt@naver.com>
2017-05-07 18:08:30 -0500

    registered jNativeHook

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,10 @@
 package application;
 	
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jnativehook.GlobalScreen;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -9,13 +14,21 @@ public class Main extends Application {
 	public void start(Stage stage) {
 		try {
 			
+			// Set GlobalScreen to only show warnings and errors.
+			Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+			logger.setLevel(Level.WARNING);
+			logger.setUseParentHandlers(false);
+			
+			// GlobalScreen
+			GlobalScreen.registerNativeHook();
+			
 			// Stage
 			stage.setTitle("AutoPerson");
 			stage.setWidth(300);
 			stage.setHeight(325);
 			stage.show();
 			stage.setOnCloseRequest(windowEvent->{
-				
+				System.exit(0);
 			});
 			
 		} catch(Exception e) {
