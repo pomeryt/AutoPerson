a7e7c2313f8973c4c3196049656627b0100d5b4d
Rin <pomeryt@naver.com>
2017-05-17 18:09:03 -0500

    initial MyArgument interface

---
+++ src/utility/MyArgument.java
@@ -0,0 +1,10 @@
+package utility;
+
+/**
+ * This interface is used to group various parameters of a method or constructor. <br />
+ * @author Rin
+ * @version 1.0.0
+ */
+public interface MyArgument {
+
+}
