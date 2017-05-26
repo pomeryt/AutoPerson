2454f7d2a2a594e383094e9fd0d733f14850fbf1
Rin <pomeryt@naver.com>
2017-05-26 08:00:37 -0500

    initial IntegerEvent

---
+++ src/utility/IntegerEvent.java
@@ -0,0 +1,14 @@
+package utility;
+
+/**
+ * This functional interface will be used for an event listener with an integer parameter.
+ * @author Rin
+ * @version 1.0.0
+ */
+public interface IntegerEvent {
+	/**
+	 * Execute something for an event where an integer parameter is required.
+	 * @param integer
+	 */
+	public void handle(int integer);
+}
